1bee5ec70891e0c8e56de50de52005d73480f375
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2018-03-24 17:29:29 +0530

    Minor style and doc fixes

--- src/bytebuf.h
+++ src/bytebuf.h
@@ -87,9 +87,9 @@ typedef struct BYTEBUF{
 /**
   * Initialize a bytebuf structure using  an externally supplied (and allocated) 
   * memory array. 
-  * @param *bufferp Pointer to the array; should be in the heap and not in a task stack.
+  * @param bufferp Pointer to the array; should be in the heap and not in a task stack.
   * @param size Size of the array. Beware : we have no good way to check this later.
-  * @param *bytebufp Pointer to the bytebuf structure to be initialized. 
+  * @param bytebufp Pointer to the bytebuf structure to be initialized. 
   */
 void bytebuf_vInit(uint8_t * bufferp, uint8_t size, bytebuf * bytebufp);
 
@@ -97,14 +97,14 @@ void bytebuf_vInit(uint8_t * bufferp, uint8_t size, bytebuf * bytebufp);
   * Flush a buffer and reset it to its pristine state.
   * @warning Any locks held on the buffer will disappear and holder is not informed.
   * @warning Any data present in the data will be lost. 
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   */
 void bytebuf_vFlush(bytebuf * bytebufp);
 
 
 /**
   * Return the number of bytes present in the buffer to be read.
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @return The number of bytes left in the buffer to be read.
   */
 static inline uint8_t bytebuf_cPopulation(bytebuf * bytebufp);
@@ -118,7 +118,7 @@ static inline uint8_t bytebuf_cPopulation(bytebuf * bytebufp){
   * Return the number of bytes that can be written into the buffer right now.
   * You still need to acquire a lock if you want to write into it. 
   * @see bytebuf_cPushReqLock()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @return The number of empty bytes left in the buffer.
   */
 static inline uint8_t bytebuf_cGetFree(bytebuf * bytebufp);
@@ -133,8 +133,8 @@ static inline uint8_t bytebuf_cGetFree(bytebuf * bytebufp){
   * You still need to acquire a lock if you want to write into it. 
   * 
   * @see bytebuf_cPushChunk()
-  * @param *bytebufp Pointer to the bytebuf structure.
-  * @param *at_rollover Pointer to uint8_t in which to store rollover flag.
+  * @param bytebufp Pointer to the bytebuf structure.
+  * @param past_rollover Pointer to uint8_t in which to store rollover flag.
   * @return The number of bytes that can be written contiguously.
   */
 static inline uint8_t bytebuf_cPushChunkLen(bytebuf * bytebufp, uint8_t * past_rollover);
@@ -158,8 +158,8 @@ static inline uint8_t bytebuf_cPushChunkLen(bytebuf * bytebufp, uint8_t * past_r
   * \brief Return the number of bytes that can be read contiguously.
   * 
   * @see bytebuf_cPopChunk()
-  * @param *bytebufp Pointer to the bytebuf structure.
-  * @param *at_rollover Pointer to uint8_t in which to store rollover flag.
+  * @param bytebufp Pointer to the bytebuf structure.
+  * @param at_rollover Pointer to uint8_t in which to store rollover flag.
   * @return The number of bytes that can be read contiguously.
   */
 static inline uint8_t bytebuf_cPopChunkLen(bytebuf * bytebufp, uint8_t * at_rollover);
@@ -184,7 +184,7 @@ static inline uint8_t bytebuf_cPopChunkLen(bytebuf * bytebufp, uint8_t * at_roll
   * Actual writing is done using other functions. 
   * @see bytebuf_cPush()
   * @see bytebuf_cPushLen()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param length Number of bytes you want to write.
   * @param token An identifier against which to issue the lock.
   * @return 0 for lock denied, 1 for lock issued.
@@ -196,7 +196,7 @@ uint8_t bytebuf_cPushReqLock(bytebuf * bytebufp, uint8_t length, uint8_t token);
   * Actual writing is done using other functions. 
   * @see bytebuf_cPush()
   * @see bytebuf_cPushLen()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param token An identifier against which to issue the lock.
   * @return 0 for lock denied, number of characters free for granted.
   */
@@ -206,7 +206,7 @@ uint8_t bytebuf_cPushReqBlindLock(bytebuf * bytebufp, uint8_t token);
   * Relinquish a write lock on a bytebuf.
   * Actual writing is done using other functions. 
   * @see bytebuf_cPushReqLock()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param token An identifier against which the lock was issued.
   * @return 0 for incorrect token, 1 for lock relinquished, 2 for already open.
   */
@@ -217,7 +217,7 @@ uint8_t bytebuf_cPushRelinquishLock(bytebuf * bytebufp, uint8_t token);
   * Write a single byte into the bytebuf, clobbering the oldest
   * value in the buffer if there isn't room. Locking isn't used.
   *
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param byte Byte to be written in.
   * @return 0 for failed write, 1 for successful write.
   */
@@ -228,7 +228,7 @@ uint8_t bytebuf_cWriteByte(bytebuf * bytebufp, uint8_t byte);
   * Push a single byte into the bytebuf.
   * You should already hold the write lock to do this.
   * @see bytebuf_cPushReqLock()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param byte Byte to be written in.
   * @param token Identifier against which you own the lock.
   * @return 0 for failed write, 1 for successful write.
@@ -239,8 +239,8 @@ uint8_t bytebuf_cPushByte(bytebuf * bytebufp, uint8_t byte, uint8_t token);
   * Push a length of data into a bytebuf. 
   * You should already hold the write lock to do this.
   * @see bytebuf_cPushReqLock()
-  * @param *bytebufp Pointer to the bytebuf structure.
-  * @param *sp Pointer to the source buffer.
+  * @param bytebufp Pointer to the bytebuf structure.
+  * @param sp Pointer to the source buffer.
   * @param len Number of bytes to push in.
   * @param token Identifier against which you own the lock.
   * @return 0 for failed write, 1 for successful write.
@@ -252,7 +252,7 @@ uint8_t bytebuf_cPushLen(bytebuf * bytebufp, void* sp, uint8_t len, uint8_t toke
   * Pop a byte from the bytebuf. 
   * You should make sure that there actually is a byte to be read first.
   * @see cbytebuf_population()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @return The byte that got popped, or 0 by default if there wasn't one. 
   * @warning return value of 0 does not necessarily mean an error occured.
   */
@@ -266,8 +266,8 @@ uint8_t bytebuf_cPopByte(bytebuf * bytebufp);
   * You should ensure that that number of bytes actually exists in the 
   * buffer to be read.
   * @see bytebuf_cPopulation()
-  * @param *bytebufp Pointer to the bytebuf structure.
-  * @param *dp Pointer to the destination buffer.
+  * @param bytebufp Pointer to the bytebuf structure.
+  * @param dp Pointer to the destination buffer.
   * @param len Number of bytes to pop.
   * @return 0 for failed read, 1 for successful read.
   */
@@ -281,8 +281,8 @@ uint8_t bytebuf_cPopLen(bytebuf * bytebufp, void* dp, uint8_t len);
   * You should ensure that that number of bytes actually exists in the 
   * buffer to be read.
   * @see bytebuf_cPopulation()
-  * @param *bytebufp Pointer to the bytebuf structure.
-  * @param *dp Pointer to the destination buffer.
+  * @param bytebufp Pointer to the bytebuf structure.
+  * @param dp Pointer to the destination buffer.
   * @param len Number of bytes to copy.
   * @return 0 for failed read, 1 for successful copy.
   */
@@ -296,7 +296,7 @@ uint8_t bytebuf_cCopyLen(bytebuf * bytebufp, void* dp, uint8_t len);
   * can be discarded from the buffer.
   * ,
   * @see bytebuf_cPopChunkLen()
-  * @param *bytebufp Pointer to the bytebuf structure.
+  * @param bytebufp Pointer to the bytebuf structure.
   * @param len The number of bytes that should be popped.
   * @return 0 if insufficient bytes to pop, the number of bytes popped 
   *         otherwise.
